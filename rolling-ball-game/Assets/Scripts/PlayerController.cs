4b6f78bc6178be7590eb3c5995568fd9ca5a7cf3
allisonchuang <allison.p.chuang.19@dartmouth.edu>
2019-01-10 11:56:58 -0500

    completed game with score and victory text

--- rolling-ball-game/Assets/Scripts/PlayerController.cs
+++ rolling-ball-game/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -29,6 +37,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
